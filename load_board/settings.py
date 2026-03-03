1858be8afce3e86514c77f8ead26c7fb7ebbe2aa
Maksym Lupekha <2627number2@gmail.com>
2026-03-03 05:28:06 -0600

    add login and logout urls

--- load_board/settings.py
+++ load_board/settings.py
@@ -126,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGIN_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
