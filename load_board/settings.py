5866d986e166bb2f2a64e1d2cd878fd764741c5d
Maksym Lupekha <2627number2@gmail.com>
2026-03-14 09:36:46 -0500

    remove SECRET_KEY from settings.py

--- load_board/settings.py
+++ load_board/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--jza7u!ga$a)!1y0+wq*=ph!1bn80n9lgyvs=luwirkb8=_*-$'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
