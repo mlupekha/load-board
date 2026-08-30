35ab987c9e727a7d8ea6086af6a4f815cdc07271
Maksym Lupekha <2627number2@gmail.com>
2026-08-30 07:11:08 -0500

    change LOGIN_REDIRECT_URL

--- config/settings/base.py
+++ config/settings/base.py
@@ -119,5 +119,5 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_URL = "/accounts/login/"
-LOGIN_REDIRECT_URL = "/accounts/login/"
+LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
